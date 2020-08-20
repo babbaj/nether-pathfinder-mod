8720bc236618f699661ba103e514d549c0561e21
Babbaj <babbaj45@gmail.com>
2020-08-20 03:55:04 -0400

    Initial commit

---
+++ src/main/java/com/babbaj/pathfinder/PathFinder.java
@@ -0,0 +1,7 @@
+package com.babbaj.pathfinder;
+
+
+public class PathFinder {
+    public static native int pathFind(long seed, int x1, int y1, int z1, int x2, int y2, int z2);
+
+}
---
+++ src/main/java/com/babbaj/pathfinder/PathFinderMod.java
@@ -0,0 +1,63 @@
+package com.babbaj.pathfinder;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+@Mod(modid = PathFinderMod.MODID, name = PathFinderMod.NAME, version = PathFinderMod.VERSION)
+public class PathFinderMod {
+    static {
+        try {
+            final InputStream libraryStream = PathFinder.class.getClassLoader().getResourceAsStream("native/libnether_pathfinder.dll");
+            Objects.requireNonNull(libraryStream, "Failed to find pathfinder library");
+            final Path tempFile = Files.createTempFile("nether_pathfinder_temp", ".dll");
+            System.out.println("Created temp file at " + tempFile.toAbsolutePath().toString());
+            try {
+                Files.copy(libraryStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
+                System.load(tempFile.toAbsolutePath().toString());
+            } finally {
+                try {
+                    Files.delete(tempFile);
+                } catch (IOException ex) {
+                    System.out.println("trolled");
+                }
+                tempFile.toFile().delete();
+                tempFile.toFile().deleteOnExit();
+            }
+
+            System.out.println("Loaded shared library");
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static final String MODID = "netherpathfinder";
+    public static final String NAME = "Nether Pathfinder";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        PathFinder.pathFind(146008555100680L, 0, 40, 0, 1000, 64, 0);
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
