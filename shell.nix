319109e25a2a516fcb80b0c4a3bcb51aeebe9100
Babbaj <babbaj45@gmail.com>
2022-08-22 19:49:16 -0400

    update nether-pathfinder

--- shell.nix
+++ shell.nix
@@ -2,14 +2,14 @@
 
 with pkgs;
 mkShell {
-  nativeBuildInputs = with pkgs; [ cmake clang_11 ];
+  nativeBuildInputs = with pkgs; [ cmake clang_14 ];
 
   #JAVA_HOME="/run/current-system/sw/lib/openjdk";
   LD_LIBRARY_PATH="${with xorg; lib.makeLibraryPath [ libXxf86vm ]}";
   JAVA_HOME="${jdk8}/lib/openjdk";
 
   shellHook = ''
-    CC=${pkgs.clang_11}/bin/cc
-    CXX=${pkgs.clang_11}/bin/c++
+    CC=${pkgs.clang_14}/bin/cc
+    CXX=${pkgs.clang_14}/bin/c++
   '';
 }
